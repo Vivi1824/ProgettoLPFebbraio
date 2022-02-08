392aa7a04b2cb9c2c4ba036c80736db607e75e41
Vivi1824 <v.giuliani3@campus.unimib.it>
2022-02-08 16:26:04 +0100

    Gestione scheme non va

--- Prolog/uri-parse.pl
+++ Prolog/uri-parse.pl
@@ -1,17 +1,34 @@
 /*l'idea è di trasformare la stringa in una lista di caratteri e mettere gli altri caratteri in un'altra lista fin quando non incontrano un simbolo speciale*/
-remove(X, [X | Xs], Y) :- remove(X, Xs, Y).
-remove(X, [Y | Ys], [Y | Z]) :- X \= Y, remove(X, Ys, Z).
-/*gestione lista*/
-gestione_scheme([Testa | Coda]) :- (Testa \= ":" -> aggiunta_lista_scheme(Lista, [Testa], ListaScheme), gestione_scheme(Coda); gestione_uri(Coda)).
-aggiunta_lista_scheme(Lista, [Testa], ListaScheme) :- append(Lista, [Testa], ListaScheme), writeln(ListaScheme).
-gestione_uri([Testa | Coda]).
-uri_parse(URIString, URI) :- string_codes(URIString, URICodeList),
-    writeln("String_codes fatto"),
-    atom_codes(URICharList, URICodeList),
-    writeln("atom_codes fatto"),
-    gestione_scheme(URICharList),
-    writeln("gestione lista fatto"),
+/*uri_parse(URIString, URI) :- string_codes(URIString, URICodeList),
+    writeln(URICodeList),
+    gestisci_codici(Lista,URICodeList),
+    writeln(Lista),
     URI = uri(Scheme, Userinfo, Host, Port, Path, Query, Fragment).
+
+gestisci_codici([Testa | Coda], []) :- Testa \= "", gestisci_codici(Coda, []).
+gestisci_codici(Lista, [Testa | Coda]) :- char_code(Char , Testa), append(Lista, [Char], Lista),
+    gestisci_codici(Lista, Coda).*/
+
+/*gestione lista*/
+/*gestione_scheme([]) :- !.
+aggiungi_elemento(Lista_Scheme,Carattere) :-
+append(Lista_Scheme,[Carattere], Lista_Scheme).*/
+uri_parse(URIString, URI) :- string_chars(URIString, ListOfChar),
+    writeln(ListOfChar),
+    gestione_scheme(ListOfChar, ListScheme),
+    writeln(ListScheme),
+    URI = uri(Scheme, Userinfo, Host, Port, Path, Query, Fragment).
+carattere(":").
+remove([], X, []) :- !.
+remove([X | T], X, L1) :- !, remove(T, X, L1).
+remove([H | T], X, [H | L1]) :- remove(T, X, L1).
+gestione_scheme([Testa | Coda], [Testa | _]) :- carattere(C), not(confronta_caratteri(C, Testa)), !, Testa1 is Testa,
+    append([Testa1 | _], [Testa], [Testa1 | Testa]).
+%aggiungere remove a gestione_scheme
+confronta_caratteri(C, Testa) :- C == Testa, !.
+
+
+
 
 
 
