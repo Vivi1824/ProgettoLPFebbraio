52fd2bab8d96b23847229675bb4076a8b0ca2942
Vivi1824 <v.giuliani3@campus.unimib.it>
2022-02-09 16:36:42 +0100

    Provare aggiunta elemento alla lista

--- Prolog/uri-parse.pl
+++ Prolog/uri-parse.pl
@@ -1,31 +1,31 @@
 /*l'idea è di trasformare la stringa in una lista di caratteri e mettere gli altri caratteri in un'altra lista fin quando non incontrano un simbolo speciale*/
-/*uri_parse(URIString, URI) :- string_codes(URIString, URICodeList),
-    writeln(URICodeList),
-    gestisci_codici(Lista,URICodeList),
-    writeln(Lista),
-    URI = uri(Scheme, Userinfo, Host, Port, Path, Query, Fragment).
-
-gestisci_codici([Testa | Coda], []) :- Testa \= "", gestisci_codici(Coda, []).
-gestisci_codici(Lista, [Testa | Coda]) :- char_code(Char , Testa), append(Lista, [Char], Lista),
-    gestisci_codici(Lista, Coda).*/
-
-/*gestione lista*/
-/*gestione_scheme([]) :- !.
-aggiungi_elemento(Lista_Scheme,Carattere) :-
-append(Lista_Scheme,[Carattere], Lista_Scheme).*/
 uri_parse(URIString, URI) :- string_chars(URIString, ListOfChar),
     writeln(ListOfChar),
-    gestione_scheme(ListOfChar, ListScheme),
-    writeln(ListScheme),
+    gestione_scheme(ListOfChar, Scheme),
+    writeln(Scheme),
     URI = uri(Scheme, Userinfo, Host, Port, Path, Query, Fragment).
-carattere(":").
-remove([], X, []) :- !.
-remove([X | T], X, L1) :- !, remove(T, X, L1).
-remove([H | T], X, [H | L1]) :- remove(T, X, L1).
-gestione_scheme([Testa | Coda], [Testa | _]) :- carattere(C), not(confronta_caratteri(C, Testa)), !, Testa1 is Testa,
-    append([Testa1 | _], [Testa], [Testa1 | Testa]).
-%aggiungere remove a gestione_scheme
-confronta_caratteri(C, Testa) :- C == Testa, !.
+% la ricorsione deve terminare quando si incontra il :, mentre prima gli
+% elementi vanno aggiunti ad una lista e rimossi dalla lista corrente
+% quindi il caso base è quando la lista è vuota oppure quando si
+% incontra :.
+lungh_lista([], 0).
+lungh_lista([_ | Coda], L) :- lungh_lista(Coda, N), L is N+1.
+/*gestione_scheme([Testa | Coda], S) :- (Testa \= ":" -> (append([Testa], [Testa], S), gestione_scheme(Coda, S)) ;
+                                      (Testa == ":" -> write(":
+                                      trovato")); !).*/
+/*gestione_scheme([Testa | Coda], [Testa]) :- Testa \= ":"; !; append([Testa], [], S), gestione_scheme(Coda, S).*/
+/*gestione_scheme([Testa | Coda], S, Scheme) :- Testa \= ":", ! , write(S), append([Testa], S, S2), gestione_scheme(Coda, S2, _).*/
+confronta_stringhe(Stringa1, ":"):- Stringa1 \= ":", writeln("true stringa").
+lista_vuota([Testa | _]) :- Testa == "", writeln("true lista vuota").
+%aggiungere aggiungi elemento lista
+agg_elemento_lista(El, ListaInput , Lista) :- append([El], ListaInput, Lista).
+gestione_scheme([Testa | Coda], Lista) :- (not(confronta_stringhe(Testa, _)) ->
+                                               (lista_vuota(Lista) -> agg_elemento_lista(Testa, Lista, Ris),
+                                                gestione_scheme(Coda, Ris);
+                                               agg_elemento_lista([Testa], Lista, Scheme),
+                                               gestione_scheme(Coda, Scheme));!).
+
+
 
 
 
